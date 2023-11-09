dfb0cf738270ef990a7ab8ca95b8f26f7f1d276c
KolbyMcGarrah <kolbymcgarrah@gmail.com>
2023-11-09 13:20:20 -0700

    fix: fixed incorrect gitignore allowing the db package to be included in the releaseJ

---
+++ internal/db/db.go
@@ -0,0 +1,95 @@
+package db
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+
+	"github.com/boltdb/bolt"
+	"github.com/kolbymcgarrah/cli-todo/internal/task"
+)
+
+type TaskDB struct {
+	db *bolt.DB
+}
+
+func OpenDB() (*TaskDB, error) {
+	db, err := bolt.Open("task.db", 0777, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+	return &TaskDB{
+		db: db,
+	}, nil
+}
+
+func (t *TaskDB) Close() error {
+	return t.db.Close()
+}
+
+func (t *TaskDB) CreateTaskBucket() error {
+	return t.db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("tasks"))
+		if err != nil {
+			return fmt.Errorf("error creating bucket: %w", err)
+		}
+		return nil
+	})
+}
+
+func (t *TaskDB) AddTask(tsk task.Task) error {
+	return t.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("tasks"))
+		id, _ := b.NextSequence()
+		tsk.ID = id
+		data, err := tsk.ToBytes()
+		if err != nil {
+			return fmt.Errorf("error converting task to bytes: %w", err)
+		}
+		err = b.Put(itob(id), data)
+		if err != nil {
+			return fmt.Errorf("error putting data in bucket: %w", err)
+		}
+		return nil
+	})
+}
+
+func itob(v uint64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, v)
+	return b
+}
+
+func bytesToTask(data []byte) (task.Task, error) {
+	var tsk task.Task
+	err := json.Unmarshal(data, &tsk)
+	if err != nil {
+		return tsk, fmt.Errorf("error unmarshaling data to task: %w", err)
+	}
+	return tsk, nil
+}
+
+func (t *TaskDB) GetTasks() ([]task.Task, error) {
+	var tasks []task.Task
+	err := t.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("tasks"))
+		return b.ForEach(func(k, v []byte) error {
+			tsk, err := bytesToTask(v)
+			if err != nil {
+				return fmt.Errorf("error parsing task: %w", err)
+			}
+			id := binary.BigEndian.Uint64(k)
+			tsk.ID = id
+			tasks = append(tasks, tsk)
+			return nil
+		})
+	})
+	return tasks, err
+}
+
+func (t *TaskDB) DeleteTask(id uint64) error {
+	return t.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("tasks"))
+		return b.Delete(itob(id))
+	})
+}
